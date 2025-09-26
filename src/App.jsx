4890e827621d4c28d30cbfeb2e9ef2c8f76546b6
Tejas Pawar <tejas.pawar@protossoft.com>
2025-09-26 09:55:32 +0530

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,43 @@ const App = () => {
         document.getElementById("responseMessage").innerText = "Error submitting login.";
       });
   }
+  function create(event) {
+    event.preventDefault();
+    const username = document.getElementById("cemail").value;
+    const password = document.getElementById("cpassword").value;
+
+    // Dummy API endpoint
+    const apiUrl = "https://server-eight-nu-19.vercel.app/api/user/create";
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: username,
+        password: password
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+
+        if (data?.isSuccess) {
+          alert(data?.message);
+          console.log("Response from API:", data);
+          // localStorage.setItem("token", data.responseDate.token);
+          // document.getElementById("responseMessage").innerText = "Logged in successfully!";
+          // window.location.href = "https://upsthitee-dev-adminapp.internal.protossoft.com/";
+        } else {
+          alert(data?.message);
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+        document.getElementById("responseMessage").innerText = "Error submitting login.";
+      });
+  }
 
   const getUsers = () => {
     fetch("https://server-eight-nu-19.vercel.app/api/user/profile").then((res) => res.JSON()).then((res) => console.log(res))
@@ -98,12 +135,12 @@ const App = () => {
           <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
             <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
               <label htmlFor="email" className="formFieldLabel">Email</label>
-              <input type="email" id="email" name="email" required />
+              <input type="email" id="cemail" name="email" required />
             </div>
 
             <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
               <label htmlFor="password" className="formFieldLabel">Password</label>
-              <input type="password" id="password" name="password" required />
+              <input type="password" id="cpassword" name="password" required />
             </div>
 
             <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
@@ -116,7 +153,7 @@ const App = () => {
             Signup
           </button>
 
-          <button className="btn-text"  onClick={getUsers}>Get Users</button>
+          <button className="btn-text"  onClick={create}>Get Users</button>
         </form>
       </div>
     </>
