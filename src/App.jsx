6d66f13e90c1c3af522c67d6c910a7ca45e484cc
Tejas Pawar <tejas.pawar@protossoft.com>
2025-09-26 08:44:52 +0530

    Test COmmit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useEffect } from "react";
+import * as Sentry from '@sentry/react';
+import neLogo from "./ne.svg";
+
+const App = () => {
+  function login(event) {
+    event.preventDefault();
+    const username = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    // Dummy API endpoint
+    const apiUrl = "http://localhost:8000/api/user/profile";
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: username,
+        password: password
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        
+        if (data?.isSuccess) {
+          alert(data?.message);
+          console.log("Response from API:", data);
+          // localStorage.setItem("token", data.responseDate.token);
+          // document.getElementById("responseMessage").innerText = "Logged in successfully!";
+          // window.location.href = "https://upsthitee-dev-adminapp.internal.protossoft.com/";
+        } else {
+          alert(data?.message);
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+        document.getElementById("responseMessage").innerText = "Error submitting login.";
+      });
+  }
+
+  const getUsers = () => {
+    fetch("http://localhost:8000/api/user/profile").then((res) => res.JSON()).then((res) => console.log(res))
+  }
+
+  return (
+    <>
+      <div className="formdiv">
+        <form id="loginForm" style={{ padding: "32px" }} onSubmit={login}>
+          <div className="form2">
+            <img src={neLogo} height="42px" width="24px" alt="Emblem" />
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <span style={{ fontWeight: 400, lineHeight: "18px" }}>महाराष्ट्र राज्य विधी सेवा</span>
+              <strong style={{ fontSize: "18px" }}>Maharashtra State Legal Services Authority</strong>
+              <span style={{ fontWeight: 400, lineHeight: "18px" }}>Government of Maharashtra</span>
+            </div>
+          </div>
+
+          <div className="formTitle">Log in to your account!</div>
+
+          <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+            <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
+              <label htmlFor="email" className="formFieldLabel">Email</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+
+            <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
+              <label htmlFor="password" className="formFieldLabel">Password</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <input type="checkbox" name="RememberMe" id="RememberMe" className="checkBoxStyle2" />
+              <label htmlFor="RememberMe" className="formFieldLabel">Remember Me</label>
+            </div>
+          </div>
+
+          <button type="submit" style={{ marginTop: "40px" }} className="btn-text">
+            Login
+          </button>
+        </form>
+
+
+        <button className="btn" onClick={getUsers}>Get Users</button>
+
+      </div>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ErrorBoundary } from "react-error-boundary";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://353ebe5cc56fae05bfa1ee29b54e0d14@o4510069292990464.ingest.us.sentry.io/4510069294694400",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <ErrorBoundary FallbackComponent={()=><h1>Error</h1>}> */}
+      <App />
+    {/* </ErrorBoundary> */}
   </React.StrictMode>
 );
 
